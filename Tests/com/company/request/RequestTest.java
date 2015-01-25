ea5e688d1c2a4a3fb60f3db7e706b21327ea9ec6
bwoodfork <byronwoodfork@gmail.com>
2015-01-24 18:08:11 -0600

    Created getters in Request for Incoming Requests

--- Tests/com/company/request/RequestTest.java
+++ Tests/com/company/request/RequestTest.java
@@ -33,19 +33,40 @@ public class RequestTest {
     public void setUp() throws Exception {
         requestBuilder = new RequestBuilder();
         request = new Request(requestBuilder);
-        fakeSocket = new FakeSocket("GET /logs HTTP/1.1Connection: closeHost: localhost:5000");
+        fakeSocket = new FakeSocket("GET /file1 HTTP/1.1Connection: closeHost: localhost:5000");
     }
     
     @Test
     public void returnsRequestAsAString() throws Exception {
-        assertEquals("GET /logs HTTP/1.1Connection: closeHost: localhost:5000", request.getRequest(fakeSocket));
+        assertEquals("GET /file1 HTTP/1.1Connection: closeHost: localhost:5000", request.getRequest(fakeSocket));
     }
     
-//    @Test
-//    public void parsesTheRequest() throws Exception {
-//        Request request = new Request(requestBuilder);
-//        RequestParser requestParser = new RequestParser()
-//
-//        assertEquals( ,request.parseRequest(fakeSocket));
-//    }
+    @Test
+    public void getsTheIncomingRequestsHTTPMethod() throws Exception {
+        Request request = new Request(requestBuilder);
+
+        assertEquals("GET", request.getMethod(fakeSocket));
+    }
+    
+    @Test
+    public void getsTheIncomingRequestsFilePath() throws Exception {
+        Request request = new Request(requestBuilder);
+        
+        assertEquals("/file1", request.getFilePath(fakeSocket));
+    }
+    
+    @Test
+    public void getsTheIncomingByteCountForRequestsThatRequireIt() throws Exception {
+        Request request = new Request(requestBuilder);
+        
+        assertEquals("closeHost:", request.getByteCount(fakeSocket));
+    }
+    
+    @Test
+    public void getsTheLocalHostDataOfTheIncomingRequest() throws Exception {
+        Request request = new Request(requestBuilder);
+        
+        assertEquals("localhost:5000", request.getData(fakeSocket));
+    }
+    
 }
--- src/com/company/request/Request.java
+++ src/com/company/request/Request.java
@@ -20,7 +20,23 @@ public class Request implements RequestInterface {
         return streamHandler.convertRawRequestToString(socket);
     }
     
-//    public RequestParser parseRequest(Socket socket) throws IOException {
-//       return new RequestParser(getRequest(socket));
-//    }
+    public RequestParser parseRequest(Socket socket) throws IOException {
+       return new RequestParser(getRequest(socket));
+    }
+    
+    public String getMethod(Socket socket) throws IOException {
+        return parseRequest(socket).getMethod();
+    }
+
+    public String getFilePath(Socket socket) throws IOException {
+        return parseRequest(socket).getFilePath();
+    }
+
+    public String getByteCount(Socket socket) throws IOException {
+        return parseRequest(socket).getByteCount();
+    }
+
+    public String getData(Socket socket) throws IOException {
+        return parseRequest(socket).getData();
+    }
 }
