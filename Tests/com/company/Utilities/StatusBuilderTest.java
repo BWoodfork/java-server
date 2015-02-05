2398f4df10987844aabb8063efdb2619035ba049
bwoodfork <byronwoodfork@gmail.com>
2015-02-05 12:47:18 -0600

    Created StatusBuilder for getting, setting and storing HTTP Status Codes

--- Tests/com/company/Utilities/StatusBuilderTest.java
+++ Tests/com/company/Utilities/StatusBuilderTest.java
@@ -1,7 +1,21 @@
 package com.company.Utilities;
 
-/**
- * Created by 8thlight on 2/5/15.
- */
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class StatusBuilderTest {
+    @Test
+    public void returnsTheHTTPStatusAfterItHasBeenSet() throws Exception {
+        StatusBuilder statusBuilder = new StatusBuilder();
+        statusBuilder.setHTTPStatus(200);
+        assertEquals("HTTP/1.1 200 OK\r\n", new String(statusBuilder.getHTTPStatus()));
+    }
+    
+    @Test
+    public void returns404NotFoundStatus() throws Exception {
+        StatusBuilder statusBuilder = new StatusBuilder();
+        statusBuilder.setHTTPStatus(404);
+        assertEquals("HTTP/1.1 404 NOT FOUND\r\n", new String(statusBuilder.getHTTPStatus()));
+    }
 }
--- src/com/company/Utilities/StatusBuilder.java
+++ src/com/company/Utilities/StatusBuilder.java
@@ -1,7 +1,23 @@
 package com.company.Utilities;
 
-/**
- * Created by 8thlight on 2/4/15.
- */
+import java.util.HashMap;
+
 public class StatusBuilder {
+    private Integer key;
+    
+    private HashMap<Integer, String> httpStatus() {
+        HashMap<Integer, String> statusSet = new HashMap<>();
+        statusSet.put(200, "200 OK");
+        statusSet.put(404, "404 NOT FOUND");
+        return statusSet;
+    }
+    
+    public void setHTTPStatus(Integer key) {
+        this.key = key;
+    }
+    
+    public byte[] getHTTPStatus() {
+        String statusString = "HTTP/1.1 " + httpStatus().get(key) + "\r\n";
+        return statusString.getBytes();
+    }
 }
