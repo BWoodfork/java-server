97754e3dd9d087e1853d44f5107ca55e44849b6f
bwoodfork <byronwoodfork@gmail.com>
2015-02-05 12:52:21 -0600

    Added funtionality to AllowMethods so it detects the requested method

--- src/com/company/Response/Headers/AllowMethods.java
+++ src/com/company/Response/Headers/AllowMethods.java
@@ -1,8 +1,24 @@
 package com.company.Response.Headers;
 
+import com.company.Utilities.FileMatcher;
+
 public class AllowMethods {
-    public byte[] getAllowResponse() {
-        String allow = "Allow: GET,HEAD,POST,OPTIONS,PUT\r\n";
-        return allow.getBytes();
+    private FileMatcher fileMatcher;
+    
+    public AllowMethods(FileMatcher fileMatcher) {
+        this.fileMatcher = fileMatcher;
+    }
+    
+    public String constructAllowHeader(String method, String requestPath) {
+        if (method.equals("GET") && requestPath.equals(fileMatcher.matchRequestedFile(requestPath))) {
+            return "Allow: GET\r\n";
+        } else if (method.equals("OPTIONS") && requestPath.equals("/method_options")) {
+            return "Allow: GET,HEAD,POST,OPTIONS,PUT\r\n";
+        } else 
+            return "Allow:\r\n";
+    }
+    
+    public byte[] getAllowResponse(String method, String requestPath) {
+        return constructAllowHeader(method, requestPath).getBytes();
     }
 }
