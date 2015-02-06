654ccd6ac36bfa4fa383627d244e773fba6893cd
bwoodfork <byronwoodfork@gmail.com>
2015-02-06 15:40:37 -0600

    Built PartialContentResponseGenerator for returning Partial Requests Body

--- Tests/com/company/Response/ResponseGenerators/PartialContentResponseGeneratorTest.java
+++ Tests/com/company/Response/ResponseGenerators/PartialContentResponseGeneratorTest.java
@@ -1,7 +1,46 @@
 package com.company.Response.ResponseGenerators;
 
-/**
- * Created by 8thlight on 2/6/15.
- */
+import com.company.Utilities.StatusBuilder;
+import com.company.request.Request;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PartialContentResponseGeneratorTest {
+    private StatusBuilder statusBuilder;
+    private PartialContentResponseGenerator partialContentResponseGenerator;
+    @Before
+    public void setUp() throws Exception {
+        statusBuilder = new StatusBuilder();
+        partialContentResponseGenerator = new PartialContentResponseGenerator(statusBuilder);
+    }
+    
+    @Test
+    public void returnsTrueIfRequestIsAPartialRequest() throws Exception {
+        Request request = new Request("GET /partial_content.txt HTTP/1.1Range: bytes=0-4Connection: closeHost: localhost:5000");
+
+        assertEquals(true, partialContentResponseGenerator.isAPartialRequest(request));
+    }
+
+    @Test
+    public void returnsFalseIfRequestIsNotAPartialRequest() throws Exception {
+        Request request = new Request("GET /partial_content.txt HTTP/1.1Range: loseHost: localhost:5000");
+
+        assertEquals(false, partialContentResponseGenerator.isAPartialRequest(request));
+    }
+    
+    @Test
+    public void returnsTheBodyWhenAPartialRequestHasBeenMade() throws Exception {
+        Request request = new Request("GET /partial_content.txt HTTP/1.1Range: bytes=0-4Connection: closeHost: localhost:5000");
+
+        assertEquals("This ", new String(partialContentResponseGenerator.getBody(request)));
+    }
+    
+    @Test
+    public void returnsErrorMessageWhenRequestIsNotAPartialRequest() throws Exception {
+        Request request = new Request("GET /partial_content.txt HTTP/1.1Range: Connection: closeHost: localhost:5000");
+
+        assertEquals("The requested endpoint does not exist", new String(partialContentResponseGenerator.getBody(request)));
+    }
 }
--- src/com/company/Response/ResponseGenerators/PartialContentResponseGenerator.java
+++ src/com/company/Response/ResponseGenerators/PartialContentResponseGenerator.java
@@ -1,7 +1,37 @@
 package com.company.Response.ResponseGenerators;
 
-/**
- * Created by 8thlight on 2/6/15.
- */
-public class PartialContentResponseGenerator {
+import com.company.Handler.PartialContentHandler;
+import com.company.Routes.IResponse;
+import com.company.Utilities.StatusBuilder;
+import com.company.request.Request;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PartialContentResponseGenerator implements IResponse {
+    private StatusBuilder statusBuilder;
+
+    public PartialContentResponseGenerator(StatusBuilder statusBuilder) {
+        this.statusBuilder = statusBuilder;
+    }
+    
+    public Path getPartialContentFilePath() throws Exception {
+        return Paths.get("../cob_spec/public/partial_content.txt");
+    }
+    
+    public boolean isAPartialRequest(Request request) throws IOException {
+        String byteCount = request.getByteCount();
+        return byteCount.startsWith("bytes=");
+    }
+    
+    public byte[] getBody(Request request) throws Exception {
+        if (isAPartialRequest(request)) {
+            statusBuilder.setHTTPStatus(206);
+            return PartialContentHandler.getPartialContents(Files.readAllBytes(getPartialContentFilePath()), request);
+        }
+
+        return "The requested endpoint does not exist".getBytes();
+    }
 }
