3f072bb7a4faf873721c4efdc604c5f5c3595692
bwoodfork <byronwoodfork@gmail.com>
2015-02-06 15:39:40 -0600

    Set HTTPStatus in ParameterResponseGenerator

--- src/com/company/Response/ResponseGenerators/ParameterIResponseGenerator.java
+++ src/com/company/Response/ResponseGenerators/ParameterIResponseGenerator.java
@@ -2,20 +2,25 @@ package com.company.Response.ResponseGenerators;
 
 import com.company.Handler.ParameterRequestHandler;
 import com.company.Routes.IResponse;
+import com.company.Utilities.StatusBuilder;
 import com.company.request.Request;
 
 import java.io.IOException;
 
 public class ParameterIResponseGenerator implements IResponse {
     private ParameterRequestHandler parameterRequestHandler;
+    private StatusBuilder statusBuilder;
     
-    public ParameterIResponseGenerator() throws IOException {
+    public ParameterIResponseGenerator(StatusBuilder statusBuilder) throws IOException {
+        this.statusBuilder = statusBuilder;
         parameterRequestHandler = new ParameterRequestHandler();
     }
     
     public byte[] getBody(Request request) throws Exception {
-        if (parameterRequestHandler.isAParameterRequest(request))
+        if (parameterRequestHandler.isAParameterRequest(request)) {
+            statusBuilder.setHTTPStatus(200);
             return parameterRequestHandler.decodeRequest(request).getBytes();
+        }
         
         return "The requested endpoint cannot be found".getBytes();
     }
