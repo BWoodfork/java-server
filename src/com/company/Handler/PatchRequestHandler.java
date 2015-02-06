7c399775b63988348643110b8ec69044a93f46b5
bwoodfork <byronwoodfork@gmail.com>
2015-02-06 15:35:56 -0600

    Refactored PatchRequestHandler, moving most of its responsibility into PatchContentResponseGenerator'

--- src/com/company/Handler/PatchRequestHandler.java
+++ src/com/company/Handler/PatchRequestHandler.java
@@ -7,32 +7,16 @@ import java.nio.file.Paths;
 
 public class PatchRequestHandler {
 
-    public PatchRequestHandler() {
-    }
-    
-    public boolean isAPatchRequest(String method) {
-       return method.equals("PATCH");
-    }
-
     public Path getPatchFilePath() throws IOException {
-        Path path = Paths.get("../cob_spec/extension-files/patch-content.txt");
+        Path path = Paths.get("../cob_spec/public/patch-content.txt");
         return path.toAbsolutePath();
     }
+
+    public void writePatchedContent() throws Exception {
+        Files.write(getPatchFilePath(), "patched content".getBytes());
+    }
     
-    public byte[] parseRequest(String method, String filePath, String data) throws Exception {
-        String[] strings = data.split("Connection:");
-        String eTagHash = strings[0];
-
-        String firstPatchString = "60bb224c68b1ed765a0f84d910de58d0beea91c4";
-        String secondPatchString = "69bc18dc1edc9e1316348b2eaaca9df83898249f";
-
-        if (method.equals("GET") && filePath.equals("/patch-content.txt")) {
-            return Files.readAllBytes(getPatchFilePath());
-        } else if (isAPatchRequest(method) && eTagHash.equals(firstPatchString)) {
-            Files.write(getPatchFilePath(), "patched content".getBytes());
-        } else if (isAPatchRequest(method) && eTagHash.equals(secondPatchString)) {
-            Files.write(getPatchFilePath(), "default content".getBytes());
-        } return "This is not the page you are looking for".getBytes();
-
+    public void writeDefaultContent() throws Exception {
+        Files.write(getPatchFilePath(), "default content".getBytes());
     }
 }
