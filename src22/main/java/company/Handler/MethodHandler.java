6644817c2c601bad2199255b622c17b66745965b
bwoodfork <byronwoodfork@gmail.com>
2015-02-12 14:03:18 -0600

    Removed old src file in place of the new.

--- src22/main/java/company/Handler/MethodHandler.java
+++
@@ -1,5 +0,0 @@
-package main.java.company.Handler;
-
-public interface MethodHandler {
-    public void execute();
-}
--- src22/main/java/company/request/RequestInterface.java
+++
@@ -1,9 +0,0 @@
-package main.java.company.request;
-
-import java.io.IOException;
-
-public interface RequestInterface {
-//    This interface will have the method(s) that are needed for getting the actual request
-    public String getRequest() throws IOException;
-    public String setStatusCode(String statusCode);
-}
