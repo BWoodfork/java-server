b911d33c02f39b23a828b2bb0e5e66f08ef2a5c1
bwoodfork <byronwoodfork@gmail.com>
2015-02-19 10:27:48 -0600

    Built MockSocket class

---
+++ src/test/me/byronwoodfork/Mocks/MockSocket.java
@@ -0,0 +1,40 @@
+package Mocks;
+
+import java.io.*;
+import java.net.Socket;
+
+public class MockSocket extends Socket {
+  public boolean isListening = true;
+  private InputStream inputStream;
+  private OutputStream outputStream;
+  
+  public MockSocket(InputStream inputStream, OutputStream outputStream) {
+    this.inputStream = inputStream;
+    this.outputStream = outputStream;
+  }
+  
+  @Override
+  public InputStream getInputStream() {
+    return inputStream;
+  }
+  
+  @Override
+  public OutputStream getOutputStream() throws IOException {
+    return outputStream;
+  }
+  
+  @Override
+  public void close() {
+    isListening = false;
+  }
+  
+  @Override
+  public boolean isClosed() {
+    return isListening;
+  }
+  
+  @Override
+  public boolean isConnected() {
+    return isListening;
+  }
+}
