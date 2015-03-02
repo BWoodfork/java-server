6f803c445c1cc876eda5ef7462dd914fffd09028
bwoodfork <byronwoodfork@gmail.com>
2015-03-02 16:39:02 -0600

    Built PatchContentHandler

---
+++ src/test/me/byronwoodfork/PatchContentHandlerTest.java
@@ -0,0 +1,53 @@
+import org.junit.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.assertEquals;
+
+public class PatchContentHandlerTest {
+  @Test
+  public void writesPatchContentToPatchContentFileWhenEtagMatches() throws Exception {
+    String testDirectoryPath = TestDirectoryPath.testDirectory;
+    PatchContentHandler patchContentHandler = new PatchContentHandler(testDirectoryPath);
+    Request request = new Request();
+    HTTPStatusCodes httpStatusCodes = new HTTPStatusCodes();
+    request.setHTTPMethod("PATCH");
+    request.setURI("patch-content.txt");
+    request.setEtag("60bb224c68b1ed765a0f84d910de58d0beea91c4");
+    patchContentHandler.buildResponse(request, httpStatusCodes);
+    
+    assertEquals("patched content", new String(Files.readAllBytes(Paths.get(testDirectoryPath + "/" + request.getURI()).toAbsolutePath())));
+    assertEquals("204 No Content", httpStatusCodes.getStatus());
+  }
+  
+  @Test
+  public void writesDefaultContentToPatchContentFileWhenEtagMatches() throws Exception {
+    String testDirectoryPath = TestDirectoryPath.testDirectory;
+    PatchContentHandler patchContentHandler = new PatchContentHandler(testDirectoryPath);
+    Request request = new Request();
+    HTTPStatusCodes httpStatusCodes = new HTTPStatusCodes();
+    request.setHTTPMethod("PATCH");
+    request.setURI("patch-content.txt");
+    request.setEtag("69bc18dc1edc9e1316348b2eaaca9df83898249fC");
+    patchContentHandler.buildResponse(request, httpStatusCodes);
+
+    assertEquals("default content", new String(Files.readAllBytes(Paths.get(testDirectoryPath + "/" + request.getURI()).toAbsolutePath())));
+    assertEquals("204 No Content", httpStatusCodes.getStatus());
+  }
+  
+  @Test
+  public void returnsDefaultContentWhenThereIsNoEtag() throws Exception {
+    String testDirectoryPath = TestDirectoryPath.testDirectory;
+    PatchContentHandler patchContentHandler = new PatchContentHandler(testDirectoryPath);
+    Request request = new Request();
+    HTTPStatusCodes httpStatusCodes = new HTTPStatusCodes();
+    request.setHTTPMethod("PATCH");
+    request.setURI("patch-content.txt");
+    request.setEtag("SomethingElse");
+    patchContentHandler.buildResponse(request, httpStatusCodes);
+    
+    assertEquals("default content", new String(Files.readAllBytes(Paths.get(testDirectoryPath + "/" + request.getURI()).toAbsolutePath())));
+    assertEquals("200 OK", httpStatusCodes.getStatus());
+  }
+}
