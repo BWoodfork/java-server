baf5b234852e227c8832e0c1d21f8ea559d88f26
bwoodfork <byronwoodfork@gmail.com>
2015-03-09 13:57:42 -0500

    Refactored ConnectionHandlerTest

--- src/test/com/httpserver/ConnectionHandlerTest.java
+++ src/test/com/httpserver/ConnectionHandlerTest.java
@@ -4,10 +4,7 @@ import com.httpserver.mocks.MockSocket;
 import com.httpserver.request.Request;
 import com.httpserver.request.RequestHandler;
 import com.httpserver.request.RequestParser;
-import com.httpserver.response.HTTPStatusCodes;
-import com.httpserver.response.Response;
 import com.httpserver.testresources.TestDirectoryPath;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
@@ -18,42 +15,51 @@ import java.io.OutputStream;
 import static org.junit.Assert.assertEquals;
 
 public class ConnectionHandlerTest {
-  private RequestHandler requestHandler;
-  private MockSocket mockSocket;
-  private InputStream inputStream;
-  private OutputStream byteArrayOutputStream;
-  private RequestParser requestParser;
-  private ConnectionHandler connectionHandler;
-  private HTTPStatusCodes httpStatusCodes;
-  private Response response;
-  
-  @Before
-  public void setUp() throws Exception {
-    inputStream = new ByteArrayInputStream("GET /file1 HTTP/1.1Connection: closeHost: localhost:5000".getBytes());
-    byteArrayOutputStream = new ByteArrayOutputStream();
-    mockSocket = new MockSocket(inputStream, byteArrayOutputStream);
-    requestHandler = new RequestHandler(mockSocket);
-    requestParser = new RequestParser(inputStream);
-    httpStatusCodes = new HTTPStatusCodes();
-    String testDirectory = TestDirectoryPath.testDirectory;
-    Routes routes = new Routes(testDirectory);
-    int port = 5000;
-    response = new Response(httpStatusCodes, routes, port);
-    connectionHandler = new ConnectionHandler(requestHandler, port, testDirectory);
-  }
-  
   @Test
   public void returnsFullRequestString() throws Exception {
+    InputStream inputStream = new ByteArrayInputStream("GET /file1 HTTP/1.1Connection: closeHost: localhost:5000".getBytes());
+    RequestParser requestParser = new RequestParser(inputStream);
+    
     Request request = requestParser.parse();
     assertEquals("GET /file1 HTTP/1.1Connection: closeHost: localhost:5000", request.getFullRequest());
   }
   
-  String textoutput = "HTTP/1.1 200 OK\r\n" + "Location: http://localhost:5000/\r\n" + "Content-Type: text/html\r\n" +
-                      "Allow: GET\r\n" + "Content-Length: 14\r\n\r\n" + "file1 contents";
   
   @Test
   public void returnsTheOutputStreamInTextWhenARequestIsMade() throws Exception {
+    InputStream inputStream = new ByteArrayInputStream("GET /file1 HTTP/1.1Connection: closeHost: localhost:5000".getBytes());
+    OutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+    MockSocket mockSocket = new MockSocket(inputStream, byteArrayOutputStream);
+    RequestHandler requestHandler = new RequestHandler(mockSocket);
+    String testDirectory = TestDirectoryPath.testDirectory;
+    int port = 5000;
+    ConnectionHandler connectionHandler = new ConnectionHandler(requestHandler, port, testDirectory);
+
+    String response = "HTTP/1.1 200 OK\r\n" + 
+        "Location: http://localhost:5000/\r\n" + 
+        "Content-Type: text/html\r\n" +
+        "Allow: GET\r\n" + "Content-Length: 14\r\n\r\n" + "file1 contents";
+    
     connectionHandler.run();
-    assertEquals(textoutput, requestHandler.getOutputStream().toString());
+    assertEquals(response, requestHandler.getOutputStream().toString());
+  }
+  
+  @Test
+  public void returnsTheOutputStreamForARequestToFile2() throws Exception {
+    InputStream inputStream = new ByteArrayInputStream("GET /file2 HTTP/1.1Connection: closeHost: localhost:5000".getBytes());
+    OutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+    MockSocket mockSocket = new MockSocket(inputStream, byteArrayOutputStream);
+    RequestHandler requestHandler = new RequestHandler(mockSocket);
+    String testDirectory = TestDirectoryPath.testDirectory;
+    int port = 5000;
+    ConnectionHandler connectionHandler = new ConnectionHandler(requestHandler, port, testDirectory);
+    
+    String response = "HTTP/1.1 200 OK\r\n" +
+        "Location: http://localhost:5000/\r\n" +
+        "Content-Type: text/html\r\n" +
+        "Allow: GET\r\n" + "Content-Length: 41\r\n\r\n" + "There is also some text in this file2 bro";
+    
+    connectionHandler.run();
+    assertEquals(response, requestHandler.getOutputStream().toString());
   }
 }
